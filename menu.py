af752ff5dcc770f78c40d3e2609b78886e1614f8
Emerson da Silva <95425629+dasilvaemerson@users.noreply.github.com>
2022-05-04 20:54:37 +0200

    Add option to generate new Bingo-Card

--- menu.py
+++ menu.py
@@ -25,8 +25,22 @@ class Menu:
         elif option == 2:
             print('Generating card...\n')
             print(card.Card.generate_card())
+            Menu.new_card()
         elif option == 3:
             print('Bye')
             sys.exit(0)
         else:
             print('Enter a valid command')
+
+    def new_card():
+        answer = "Y"
+        while answer == "Y":
+            print("Generate new Bingo Card?")
+            answer = input("Y/N: ").upper()
+            if answer == "Y":
+                print(card.Card.generate_card())
+            elif answer == "N":
+                Menu.start()
+            else:
+                print("#!# Invalid answer. Try again #!#")
+                answer = "Y"
